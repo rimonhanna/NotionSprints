a06d2a2c5176352c7471d466287edbd7e82ecc99
Rimon Hanna <rimon.hanna@live.com>
2021-10-05 17:14:35 +0200

    Update sprints.py

--- sprints.py
+++ sprints.py
@@ -13,7 +13,7 @@ client = NotionClient(token_v2=token)
 sprints = client.get_collection_view(os.getenv('SPRINTS_TABLE_URL'))
 
 active_sprints = [x for x in sprints.collection.get_rows() if x.active_sprint]
-next_sprints = [x for x in sprints.collection.get_rows() if x.start_date and x.end_date and (x.start_date.start <= datetime.today().date() and x.end_date.start >= datetime.today().date())]
+next_sprints = [x for x in sprints.collection.get_rows() if x.start_date and x.end_date and (x.start_date.start <= datetime.today().date() and x.end_date.start > datetime.today().date())]
 
 def is_task_done(task): 
     return task.status in ["Demo", "Done 🙌"]
